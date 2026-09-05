Fixed: wrong shape of layer norm's learning paramater gamma

unit_test/binary_reader/autograd_binary_reader.py
@@ -120,13 +120,22 @@ def ReaderFlashAttention(
                           ).to(device)
 
     layer_norm_gamma = load_tensor("./src/cache/cpp_out/gamma_host.bin",
-                        shape=(batch_size, 1, 1),
+                        shape=(d_model, 1, 1),
                         dtype=np.float32
                         ).to(device)
     x = load_tensor("./src/cache/cpp_out/embedding.bin",
                     shape=(batch_size, seq_len, d_model),
                     dtype=np.float32).to(device)
-    return P, V, PT, VT, G_unc, dl_dh, dp, dV, softmax_upstream, dQ, k, q, d_score_t, dK, wqt, wkt, wvt, wq, wk, wv, upq, upk, upv, G_x_hat, layer_norm_gamma, mean_cache, std_dev_cache, x
+
+    layer_norm_back_x = load_tensor("./src/cache/cpp_out/layer_norm_x.bin",
+                                    shape=(batch_size, seq_len, d_model),
+                                    dtype=np.float32).to(device)
+
+    return (P, V, PT, VT, G_unc, dl_dh, dp, dV,
+            softmax_upstream, dQ, k, q, d_score_t,
+            dK, wqt, wkt, wvt, wq, wk, wv, upq, upk,
+            upv, G_x_hat, layer_norm_gamma, mean_cache,
+            std_dev_cache, x, layer_norm_back_x)
 
 def Reader(
         batch_size: int,

unit_test/debug/debug_flash_attention.py
@@ -19,7 +19,15 @@ class DebugFlashAttention(torch.nn.Module):
         self.dl_dw = dl_dw # Upstream gradient G
 
         # looks like ideal gas equation, but it's not
-        self.P, self.V, self.PT, self.VT, self.G_unc, self.G, self.dp, self.dv, self.softmax_upstream, self.dQ, self.K, self.Q, self.d_score_t, self.dK, self.wqt, self.wkt, self.wvt, self.wq, self.wk, self.wv, self.upq, self.upk, self.upv, self.G_x_hat, self.layer_norm_gamma, self.mc, self.stdc, self.x = ReaderFlashAttention(batch_size, seq_len, vocab_size, d_model, num_heads, head_dim)
+        (self.P, self.V, self.PT,
+         self.VT, self.G_unc, self.G,
+         self.dp, self.dv, self.softmax_upstream,
+         self.dQ, self.K, self.Q, self.d_score_t,
+         self.dK, self.wqt, self.wkt, self.wvt,
+         self.wq, self.wk, self.wv, self.upq,
+         self.upk, self.upv, self.G_x_hat,
+         self.layer_norm_gamma, self.mc, self.stdc,
+         self.x, self.layer_norm_back_x) = ReaderFlashAttention(batch_size, seq_len, vocab_size, d_model, num_heads, head_dim)
 
     # dV = P^T G
     # dP = GV^T
@@ -184,7 +192,7 @@ class DebugFlashAttention(torch.nn.Module):
         else:
             print(f"Checking G_x_hat (total upstream for layer norm), status: {GREEN} {check_G_x_hat} {RESET}")
 
-        layer_backward_analytical(
+        back_x = layer_backward_analytical(
             self.x,
             self.G_x_hat,
             self.layer_norm_gamma,
@@ -194,3 +202,6 @@ class DebugFlashAttention(torch.nn.Module):
             self.seq_len,
             self.d_model
         )
+
+        print(f"From kernel: {self.layer_norm_back_x}")
+        print(f"From torch: {back_x}")